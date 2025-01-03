3a0af3861ac402a6a25f79b5de7f8cd78eefd9b3
EWANZO101 <107235474+EWANZO101@users.noreply.github.com>
2025-01-03 23:06:16 +0000

    1

---
+++ client.lua
@@ -0,0 +1,33 @@
+local reminderInterval = 60000 -- 60 seconds
+local popupVisible = false
+
+-- Show Popup
+local function showPopup()
+    if not popupVisible then
+        popupVisible = true
+        SetNuiFocus(true, true) -- Enable mouse and keyboard input
+        SendNUIMessage({ action = "openPopup" }) -- Trigger NUI popup
+    end
+end
+
+-- Close Popup
+RegisterNUICallback("closePopup", function(data, cb)
+    popupVisible = false
+    SetNuiFocus(false, false) -- Disable input focus
+    cb("ok")
+end)
+
+-- Periodic Reminder
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(reminderInterval)
+        showPopup()
+    end
+end)
+
+-- Manual Command to Test Popup
+RegisterCommand("apiReminder", function()
+    showPopup()
+end, false)
+
+print("[DEBUG] API Reminder Script Loaded Successfully!")
